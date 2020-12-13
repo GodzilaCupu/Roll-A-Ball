611248465dab0e405468d9ba897e0468339a179a
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-14 06:12:57 +0700

    Finale

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    [SerializeField]
+    private AudioSource touchedSound;
+
+    private float movementX;
+    private float movementY;
+    private int count;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        // Triggered when get touch by coin
+        if (trigger.gameObject.tag == "Coin")
+        {
+            Debug.Log("Coin touched");
+            touchedSound.Play();
+            trigger.gameObject.SetActive(false);
+            // Add one to the score variable 'count'
+            count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 7)
+        {
+            // Set the text value of your 'winText' 
+            SceneManager.LoadScene(1);
+            Time.timeScale = 0f;
+        }
+    }
+
+}
